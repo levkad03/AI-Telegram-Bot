17fdfd13304d0eafe69a4fb7ce76a36bb0cdd67d
Lev Kadatskyi <levkadatscky@gmail.com>
2024-02-06 11:02:50 +0200

    changed message at /start

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ async def command_start_handler(message: Message) -> None:
     # and the target chat will be passed to :ref:`aiogram.methods.send_message.SendMessage`
     # method automatically or call API method directly via
     # Bot instance: `bot.send_message(chat_id=message.chat.id, ...)`
-    await message.answer(f"Hello, {hbold(message.from_user.full_name)}, I am Animal Classifier! I can classify some animals on the images.")
+    await message.answer(f"Hello, {hbold(message.from_user.full_name)}, I am Animal Classifier! I can classify some animals on the images. I can classify such animals as cat, dog, elephant, horse and lion")
 
 
 @router.message(Command("help"))
